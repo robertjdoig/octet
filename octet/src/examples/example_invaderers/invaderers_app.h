0be2be138f13b45b66444f04edddb00a5a626e36
MSI\robert <robertjdoig@hotmail.co.uk>
2016-10-21 13:07:47 +0100

    Adding collisions with the missle and block

--- octet/src/examples/example_invaderers/invaderers_app.h
+++ octet/src/examples/example_invaderers/invaderers_app.h
@@ -25,123 +25,9 @@
 
 #include "sprite.h"
 
+
 namespace octet {
-  //class sprite {
-  //  // where is our sprite (overkill for a 2D game!)
-  //  mat4t modelToWorld;
-
-  //  // half the width of the sprite
-  //  float halfWidth;
-
-  //  // half the height of the sprite
-  //  float halfHeight;
-
-  //  // what texture is on our sprite
-  //  int texture;
-
-  //  // true if this sprite is enabled.
-  //  bool enabled;
-  //public:
-  //  sprite() {
-  //    texture = 0;
-  //    enabled = true;
-  //  }
-
-  //  void init(int _texture, float x, float y, float z, float w, float h) {
-  //    modelToWorld.loadIdentity();
-  //    modelToWorld.translate(x, y, z);
-  //    halfWidth = w * 0.5f;
-  //    halfHeight = h * 0.5f;
-  //    texture = _texture;
-  //    enabled = true;
-  //  }
-
-  //  void render(texture_shader &shader, mat4t &cameraToWorld) {
-  //    // invisible sprite... used for gameplay.
-  //    if (!texture) return;
-
-  //    // build a projection matrix: model -> world -> camera -> projection
-  //    // the projection space is the cube -1 <= x/w, y/w, z/w <= 1
-  //    mat4t modelToProjection = mat4t::build_projection_matrix(modelToWorld, cameraToWorld);
-
-  //    // set up opengl to draw textured triangles using sampler 0 (GL_TEXTURE0)
-  //    glActiveTexture(GL_TEXTURE0);
-  //    glBindTexture(GL_TEXTURE_2D, texture);
-
-  //    // use "old skool" rendering
-  //    //glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST_MIPMAP_NEAREST);
-  //    //glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-  //    shader.render(modelToProjection, 0);
-
-  //    // this is an array of the positions of the corners of the sprite in 3D
-  //    // a straight "float" here means this array is being generated here at runtime.
-  //    float vertices[] = {
-  //      -halfWidth, -halfHeight, 0,
-  //       halfWidth, -halfHeight, 0,
-  //       halfWidth,  halfHeight, 0,
-  //      -halfWidth,  halfHeight, 0,
-  //    };
-
-  //    // attribute_pos (=0) is position of each corner
-  //    // each corner has 3 floats (x, y, z)
-  //    // there is no gap between the 3 floats and hence the stride is 3*sizeof(float)
-  //    glVertexAttribPointer(attribute_pos, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)vertices);
-  //    glEnableVertexAttribArray(attribute_pos);
-
-  //    // this is an array of the positions of the corners of the texture in 2D
-  //    static const float uvs[] = {
-  //      0,  0,
-  //      1,  0,
-  //      1,  1,
-  //      0,  1,
-  //    };
-
-  //    // attribute_uv is position in the texture of each corner
-  //    // each corner (vertex) has 2 floats (x, y)
-  //    // there is no gap between the 2 floats and hence the stride is 2*sizeof(float)
-  //    glVertexAttribPointer(attribute_uv, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void*)uvs);
-  //    glEnableVertexAttribArray(attribute_uv);
-
-  //    // finally, draw the sprite (4 vertices)
-  //    glDrawArrays(GL_TRIANGLE_FAN, 0, 4);
-  //  }
-
-  //  // move the object
-  //  void translate(float x, float y, float z) {
-  //    modelToWorld.translate(x, y, z);
-  //  }
-
-  //  // position the object relative to another.
-  //  void set_relative(sprite &rhs, float x, float y, float z) {
-  //    modelToWorld = rhs.modelToWorld;
-  //    modelToWorld.translate(x, y, z);
-  //  }
-
-  //  // return true if this sprite collides with another.
-  //  // note the "const"s which say we do not modify either sprite
-  //  bool collides_with(const sprite &rhs) const {
-  //    float dx = rhs.modelToWorld[3][0] - modelToWorld[3][0];
-  //    float dy = rhs.modelToWorld[3][1] - modelToWorld[3][1];
-
-  //    // both distances have to be under the sum of the halfwidths
-  //    // for a collision
-  //    return
-  //      (fabsf(dx) < halfWidth + rhs.halfWidth) &&
-  //      (fabsf(dy) < halfHeight + rhs.halfHeight);
-  //  }
-
-  //  bool is_above(const sprite &rhs, float margin) const {
-  //    float dx = rhs.modelToWorld[3][0] - modelToWorld[3][0];
-
-  //    return
-  //      (fabsf(dx) < halfWidth + margin);
-  //  }
-
-  //  bool &is_enabled() {
-  //    return enabled;
-  //  }
-  //};
-
+ 
   class invaderers_app : public octet::app {
        
     // Matrix to transform points in our camera space to the world.
@@ -365,7 +251,7 @@ namespace octet {
         // find a missile
         for (int i = 0; i != num_missiles; ++i) {
           if (!sprites[first_missile_sprite + i].is_enabled()) {
-            sprites[first_missile_sprite + i].set_relative(sprites[ship_sprite], 0, 0.5f, 0);
+            sprites[first_missile_sprite + i].set_relative(sprites[ship_sprite], 0, 0, 0);
             sprites[first_missile_sprite + i].is_enabled() = true;
             missiles_disabled = 5;
             ALuint source = get_sound_source();
@@ -408,10 +294,7 @@ namespace octet {
 
     // animate the missiles
     void bomb_timer() {
-
-
-      /*
-      const float missile_speed = 0.3f;
+      const float missile_speed = 0.1f;
       for (int i = 0; i != num_missiles; ++i) {
         sprite &missile = sprites[first_missile_sprite + i];
         if (missile.is_enabled()) {
@@ -429,6 +312,19 @@ namespace octet {
               goto next_missile;
             }
           }
+          for (int j = 0; j != num_blocks; ++j) {
+            sprite &block = sprites[first_block_sprite + j];
+            if (missile.collides_with(block)) {
+             // block.is_enabled() = false;
+             // block.translate(20, 0, 0);
+              missile.is_enabled() = false;
+              missile.translate(20, 0, 0);
+              
+            
+
+              goto next_missile;
+            }
+          }
           if (missile.collides_with(sprites[first_border_sprite + 1])) {
             missile.is_enabled() = false;
             missile.translate(20, 0, 0);
@@ -436,7 +332,6 @@ namespace octet {
         }
       next_missile:;
       }
-      */
     }
 
     // animate the bombs
@@ -596,11 +491,11 @@ namespace octet {
       
 
       // use the missile texture
-      GLuint missile = resource_dict::get_texture_handle(GL_RGBA, "assets/invaderers/missile.gif");
+      GLuint missile = resource_dict::get_texture_handle(GL_RGBA, "assets/invaderers/bomb_circle.gif");
       //missile = resource_dict::get_texture_handle(GL_RGBA, "#fff200");
       for (int i = 0; i != num_missiles; ++i) {
         // create missiles off-screen
-        sprites[first_missile_sprite + i].init(missile, 20, 0, 0, 0.0625f, 0.25f);
+        sprites[first_missile_sprite + i].init(missile, 20, 0, 0, 0.15f, 0.15f);
         sprites[first_missile_sprite + i].is_enabled() = false;
       }
 
@@ -643,7 +538,7 @@ namespace octet {
 
       bomb_timer();
 
-     // move_bombs();
+      move_bombs();
 
      // move_invaders(invader_velocity, 0, 0);
 
